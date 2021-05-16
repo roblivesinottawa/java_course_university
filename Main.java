9617be5efc55177037482ecf58ce1a4c59b76b22
Robson Ferreira <tech.rob@icloud.com>
2021-05-16 15:35:31 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+// package com.company;
+
+// public class Main {
+
+//     public static void main(String[] args) {
+//         int age = 34;
+//         int newAge = age;
+// 	System.out.println(newAge);
+//     }
+// }
+
+
+
---
+++ Ref.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Ref {
+    public static void ref(String[] args) {
+        byte age = 30;
+        Date now = new Date();
+        // now.getTime();
+        System.out.println(now);
+    }
+    
+}
